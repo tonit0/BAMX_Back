0fe2e84cadf25ec6243f4ecd3b5b20d19848e4f9
Jonathan Padilla <jonathan_pad22@hotmail.com>
2023-05-16 21:17:12 -0700

    estructura

---
+++ src/index.ts
@@ -0,0 +1,27 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import routes from "./routes/index";
+
+const PORT = process.env.PORT || 3001; 
+const app = express();
+
+app.use((_req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use('/', routes);
+
+app.listen(PORT, () => {
+    console.log('Server on port ', PORT);
+})
